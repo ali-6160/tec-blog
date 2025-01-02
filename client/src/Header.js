f1b93335c8837f32158c49c000eca284d9f90f9c
Ali <aliasgharfaridi5@gmail.com>
2025-01-02 22:17:48 +0100

    #

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
 
-      <a href = "" className = "logr">MyBlog</a>
+      <Link to = "/" className = "logo">MyBlog</Link>
       <nav>
-        <a href ="" >Login</a>
-        <a href ="" >Register</a>
+        <Link to ="/login" >Login</Link>
+        <Link to ="/register" >Register</Link>
       </nav>  
       </header>
   );
