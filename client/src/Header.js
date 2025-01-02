1c086e65672f7a62f98db148465de9dc10165c2d
Ali <aliasgharfaridi5@gmail.com>
2025-01-02 19:35:51 +0100

    #

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+
+      <a href = "" className = "logr">MyBlog</a>
+      <nav>
+        <a href ="" >Login</a>
+        <a href ="" >Register</a>
+      </nav>  
+      </header>
+  );
+}
---
+++ client/src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return (
+        <div className = "post">
+        <div className = "image">
+          <img src = "https://techcrunch.com/wp-content/uploads/2024/12/data-breach-2024-roundup.jpg?resize=1097,617" alt = "image"/>
+        
+        </div>
+        <div className = "texts">
+          <h2>These were the badly handled data breaches of 2024</h2>
+          <p className = "info">
+            <a className = "autor">Ali Faridi</a>
+            <time>2025-01-02 18:52</time>
+          </p>
+          <p>For the past few years, TechCrunch has looked back at some of the worst, badly handled data breaches and security incidents in the hope — maybe! — other corporate giants would take heed and avoid making some of the same calamities of yesteryear.</p>
+        
+        </div>
+        </div>
+    );
+    }
