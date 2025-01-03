3fc76efe5ac373d83a552a938c5c8bef0a4c7b6b
Ali <aliasgharfaridi5@gmail.com>
2025-01-03 14:43:33 +0100

    #

--- api/index.js
+++ api/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+ mongoose.connect('mongodb+srv://faridialiasghar5:9Z4CAhZddcPd7tqd@cluster0.st02y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({requestData: {username, password}});
+    try{
+ const userDoc = await User.create({ 
+    username,
+    password: bcrypt.hashSync(password, salt)
+ });
+} catch(e){
+    res.status(400).json({e});
+}
     }) 
 
 
 app.listen(4000)
+
+// mongodb+srv://faridialiasghar5:9Z4CAhZddcPd7tqd@cluster0.st02y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// 9Z4CAhZddcPd7tqd
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -2,23 +2,39 @@ import React, { useState } from "react";
 export default function RegisterPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    async function register (ev){
+    async function register(ev) {
         ev.preventDefault();
-         await fetch("http://localhost:4000/register", {
+
+        const response = await fetch("http://localhost:4000/register", {
             method: "POST",
-            body: JSON.stringify({username, password}),
+            body: JSON.stringify({ username, password }),
             headers: {
-                "Content-Type": "application/json"
-            }
-        })
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.status === 200) {
+            alert("Register success");
+        } else {
+            alert("Register faild");
+        }
     }
     return (
         <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="Username" value={username} onChange={ev => setUsername(ev.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={ev => setPassword(ev.target.value)} />
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(ev) => setUsername(ev.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+            />
             <button>Register</button>
-
-          </form>
+        </form>
     );
 }
